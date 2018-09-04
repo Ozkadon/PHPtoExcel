a66b6da5ed6acd7c66093ce60e7710b5adc15057
VERMOUTH\HP <oeidonny.winarto@gmail.com>
2018-09-04 13:53:58 +0700

    1.0

---
+++ data.php
@@ -0,0 +1,44 @@
+<table border="1">
+	<tr>
+		<th>NO.</th>
+		<th>NAMA LENGKAP</th>
+		<th>KELAS</th>
+		<th>JURUSAN</th>
+	</tr>
+    <tr>
+        <td>1</td>
+        <td>Donny A</td>
+        <td>1</td>
+        <td>Jurusan A</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Donny B</td>
+        <td>2</td>
+        <td>Jurusan B</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Donny C</td>
+        <td>3</td>
+        <td>Jurusan C</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Donny D</td>
+        <td>4</td>
+        <td>Jurusan D</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>Donny E</td>
+        <td>5</td>
+        <td>Jurusan E</td>
+    </tr>
+    <tr>
+        <td>6</td>
+        <td>Donny F</td>
+        <td>6</td>
+        <td>Jurusan F</td>
+    </tr>
+</table>
